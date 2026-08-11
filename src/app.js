9644bf78fbf4c2beb16eac8d1b1ef1b23eb51016
Aresha-Khalid072 <areshakhalid072@gmail.com>
2026-08-11 13:06:36 +0500

    Utils code discussion

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+const app=express();
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
+// app.use ata hai configuration yahh middleware k lieyy
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+export {app}
