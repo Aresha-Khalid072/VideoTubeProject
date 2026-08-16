6f7f1d745271c234e094724e70be8b80df045c7d
Aresha-Khalid072 <areshakhalid072@gmail.com>
2026-08-16 12:25:57 +0500

    Refresh and Access Token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,8 @@
 import {Router} from "express"
-import { registerUser } from "../controllers/user.controller.js"
+import { loginUser, logOutUser, registerUser } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 
-
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router=Router()
 
@@ -23,5 +23,12 @@ router.route("/register").post(
     registerUser
 
 )
+
+router.route("/login").post(loginUser)
+
+
+//secured routes
+router.route("/logout").post(verifyJWT,logOutUser)
+
 
 export default router
